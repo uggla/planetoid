83e7ec299c58faf2e46a757ff70a935010f929d4
Uggla <uggla@free.fr>
2021-05-23 23:57:35 +0200

    feat: Server
    - Add server files to server directory
    - The server is a Quarkus application using websockets derive from the
      chat example.

---
+++ server/src/main/java/fr/uggla/ChatSocket.java
@@ -0,0 +1,63 @@
+package fr.uggla;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import javax.websocket.Session;
+
+@ServerEndpoint("/chat/{username}")
+@ApplicationScoped
+public class ChatSocket {
+
+    Map<String, Session> sessions = new ConcurrentHashMap<>();
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) {
+        sessions.put(username, session);
+        broadcast("User " + username + " joined");
+    }
+
+    @OnClose
+    public void onClose(Session session, @PathParam("username") String username) {
+        sessions.remove(username);
+    }
+
+    @OnError
+    public void onError(Session session, @PathParam("username") String username, Throwable throwable) {
+        sessions.remove(username);
+        broadcast("User " + username + " left on error: " + throwable);
+    }
+
+// Example using binary message
+//    @OnMessage
+//    public void onMessage(ByteBuffer message, @PathParam("username") String username) {
+//        String msg = StandardCharsets.UTF_8.decode(message).toString();
+//        System.out.println("Received msg:" + msg );
+//        broadcast(">> " + username + ": " + msg);
+//     }
+    @OnMessage
+    public void onMessage(String message, @PathParam("username") String username) {
+        System.out.println("Received msg:" + message);
+//        broadcast(">> " + username + ": " + message);
+        broadcast(message);
+    }
+
+    private void broadcast(String message) {
+        sessions.values().forEach(s -> {
+            s.getAsyncRemote().sendObject(message, result -> {
+                if (result.getException() != null) {
+                    System.out.println("Unable to send message: " + result.getException());
+                }
+            });
+        });
+    }
+}
---
+++ server/src/test/java/fr/uggla/NativeplanetoidIT.java
@@ -0,0 +1,9 @@
+package fr.uggla;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeplanetoidIT extends planetoidTest {
+
+    // Execute the same tests but in native mode.
+}
